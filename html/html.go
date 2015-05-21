f2852396a5225ed85034a2a2c6349b604e84e993
mengqi <5b5f7426@gmail.com>
2015-05-21 15:44:54 +0800

    add html.AbsUrl(), html.AbsUrlHtml()

--- html/html.go
+++ html/html.go
@@ -2,6 +2,9 @@ package html
 
 import "html"
 import "strings"
+import "net/url"
+import "bytes"
+import "github.com/PuerkitoBio/goquery"
 
 /* Convert text to html
 
@@ -49,5 +52,77 @@ func Text2Html(text string) string {
     }
 
     return strings.Join(h, "")
+}
+
+
+// Convert a relative url to an absolute url base on a base url.
+// If rurl is an absolute url, the function just return it.
+func AbsUrl(baseurl, rurl string) (u string, err error) {
+
+    base, err := url.Parse(baseurl)
+    if err != nil {
+        return
+    }
+
+    r, err := url.Parse(rurl)
+    if err != nil {
+        return
+    }
+
+    u = base.ResolveReference(r).String()
+    return
+}
+
+
+/* Convert an html doc's urls from relative to absolute base on a base url.
+The function convert urls of the following tags and attributes:
+    tag:a, attribute: href
+    tag:img, attribute: src
+If you want to add more tags, just set the customTags parameter.
+*/
+func AbsUrlHtml(baseurl, htmlstr string, customTags ...map[string]string) (h string, err error) {
+
+    base, err := url.Parse(baseurl)
+    if err != nil {
+        return
+    }
+
+    var tags map[string]string
+
+    if len(customTags) > 0 && customTags[0] != nil {
+        tags = customTags[0]
+    } else {
+        // default tags
+        tags = make(map[string]string)
+        tags["a"] = "href"
+        tags["img"] = "src"
+    }
+
+    doc, err := goquery.NewDocumentFromReader(bytes.NewReader([]byte(htmlstr)))
+    if err != nil {
+        return
+    }
+
+    for key, value := range tags {
+        sel := doc.Find(key)
+        for i:= range sel.Nodes {
+            single := sel.Eq(i)
+            original_url, ok := single.Attr(value)
+            if !ok {
+                continue
+            }
+
+            ourl, err := url.Parse(original_url)
+            if err != nil {
+                continue
+            }
+
+            single.SetAttr(value, base.ResolveReference(ourl).String())
+        }
+    }
+
+    err = nil
+    h, err = doc.Html()
+    return
 }
 
