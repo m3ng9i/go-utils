4afa3f68652f4b5470e78368c8751c2ced85a6c6
mengqi <5b5f7426@gmail.com>
2015-07-06 14:41:18 +0800

    add package encoding

--- xml/xml.go
+++ xml/xml.go
@@ -12,8 +12,8 @@ This function will solve the problem.
 The list of valid characters is in the XML specification:
 http://www.w3.org/TR/xml/#charsets
 
-Valid characters: 
-#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]	
+Valid characters:
+#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
 */
 func RemoveInvalidChars(b []byte) []byte {
     re := regexp.MustCompile("[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]")
